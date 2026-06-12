6e277528f613e0072f1c31d26cf18dbe2dcf2652
Alex Jo Bobby <alexjocoding@gmail.com>
2026-06-12 13:38:17 +0530

    Add: string-encode-and-decode - submission-8

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-8.java
@@ -0,0 +1,30 @@
+
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder res = new StringBuilder();
+        for (String str : strs) {
+            res.append(str.length()).append("`").append(str);
+        }
+        return res.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> ans = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+           
+            int j = i;
+            while (str.charAt(j) != '`') {
+                j++;
+            }
+
+            int length = Integer.parseInt(str.substring(i, j));
+            String currentStr = str.substring(j + 1, j + 1 + length);
+            ans.add(currentStr);
+
+            i = j + 1 + length;
+        }
+        return ans;
+    }
+}
